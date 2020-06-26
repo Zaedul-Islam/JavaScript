f68207161111d975098fdcb9fd76cab25bd30465
Zaedul Islam <islam.zaedul@gmail.com>
2020-06-26 23:49:17 +0600

    Add pipeline methods

---
+++ JavaScript Array reduce()/Pipeline with reduce methos.js
@@ -0,0 +1,7 @@
+function increment(input) { return input + 1; }
+
+function decrement(input) { return input - 1; }
+
+function double(input) { return input * 2; }
+
+function halve(input) { return input / 2; }
