9a54bb8672e4595d6abcb7fe789eb594cb47391b
Zaedul Islam <islam.zaedul@gmail.com>
2021-02-06 17:06:24 +0600

    Update Capitalize First Word.js

--- Capitalize First Word.js
+++ Capitalize First Word.js
@@ -1,7 +1,7 @@
 /**
 A shortcut way to capitalize all characters of the first word of a sentence
 We'll make use of the 'Spread Operator' and 'Destructuring Assignment' to implement the task
- */
+*/
 
 function upperFirstWord(sentence){
     const [firstWord, ...otherWords] = sentence.split(' ');
